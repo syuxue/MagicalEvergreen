92ad8075dd90afcbff1f30341f19efe23ddd93ea
ShiNey <syuxue@hotmail.com>
2013-08-28 10:46:15 +0800

    Basic01
    Try to use C# and test for some IO function

---
+++ src/Mainform.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace src
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(358, 633);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(437, 292);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(144, 186);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(723, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TestLoad";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(550, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 57);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "TestWrite";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(666, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(666, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 33);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "TestSearch";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(547, 184);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(282, 84);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(364, 292);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 47);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Trans";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(616, 292);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(163, 186);
+            this.listBox2.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(438, 487);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(81, 28);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Remove";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(616, 487);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(96, 28);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "<-Add";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 633);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Mainform";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Mainform_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ src/Mainform.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace src
+{
+
+
+    public partial class Mainform : Form
+    {
+        static int FileLenthUpper = 20000;
+        String[] en_set = { "log", "cutgrass", "rocks", "charcoal" };
+        String[] cn_set = { "木头", "草", "岩石", "木炭" };
+
+        //direction=T-> EN->CN, F-> CN->EN 
+        public string trans(string str, bool direction)
+        {
+            int i = 0;
+
+            if (direction)
+            {
+                for (i=0;i < en_set.Length;i++)
+                {
+                    if (en_set[i] == str)
+                        return cn_set[i];
+                }
+                return str;
+            }
+            else
+            {
+                for (i = 0; i < cn_set.Length; i++)
+                {
+                    if (cn_set[i] == str)
+                        return en_set[i];
+                }
+                return str;
+            }
+        }
+
+        public Mainform()
+        {
+            InitializeComponent();
+        }
+
+        private void Mainform_Load(object sender, EventArgs e)
+        {
+        }
+
+       private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] byData = new byte[FileLenthUpper];
+            char[] byChar = new char[FileLenthUpper];
+
+            try
+            {
+                FileStream aFile = new FileStream("evergreens.lua", FileMode.Open);
+                //FileStream aFile = new FileStream("test.txt", FileMode.Open);
+                //aFile.Seek(1, SeekOrigin.Begin);
+                aFile.Read(byData, 0, (int)aFile.Length);
+                aFile.Close();
+            }
+            catch (IOException err)
+            {
+                MessageBox.Show("An IO error has been thrown!");
+                MessageBox.Show("Error Message:\n" + err.ToString());
+                return;
+            }
+
+            Decoder d = Encoding.UTF8.GetDecoder();
+            d.GetChars(byData, 0, byData.Length, byChar, 0);
+            string str = new string(byChar);
+            textBox1.Text = str ;
+ 
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] byData;//=new byte[FileLenthUpper];
+            char[] byChar;//=new char[FileLenthUpper];
+
+            try
+            {
+                FileStream aFile = new FileStream("testwrite.txt", FileMode.Create);
+                byChar = textBox1.Text.ToCharArray();
+                //byChar = "test string.".ToCharArray();
+                byData = new byte[byChar.Length];
+                Encoder en = Encoding.UTF8.GetEncoder();
+                en.GetBytes(byChar, 0, byChar.Length, byData, 0, true);
+
+                //begin to write
+                aFile.Seek(0, SeekOrigin.Begin);
+                aFile.Write(byData, 0, byData.Length);
+                aFile.Close();
+            }
+            catch(IOException err) 
+            { 
+                MessageBox.Show("An IO error has been thrown!");
+                MessageBox.Show("Error Message:\n" + err.ToString());
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string tofind;
+            string wholeStr = textBox1.Text;
+            tofind = textBox2.Text;
+            int beginpos = 0;
+            int current_pos = 0;
+            current_pos = wholeStr.IndexOf(tofind, current_pos);
+            if (current_pos == -1)
+                textBox2.Text = "";
+            else
+                textBox3.Text = wholeStr.Substring(current_pos);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string tofind;
+            string wholeStr = textBox1.Text;
+			string dropthing, dropthing_transed;
+            int pos_begin = 0;
+            int pos_left = 0;
+            int pos_right = 0;
+            
+            tofind = "inst.components.lootdropper:SpawnLootPrefab";
+            //locate in the function
+            pos_begin = wholeStr.IndexOf("local function chop_down_burnt_tree(inst, chopper)");
+            //ready
+            listBox1.Items.Clear();
+            //search
+            while ((pos_begin = wholeStr.IndexOf(tofind, pos_begin+1)) != -1)
+            {
+                pos_left = wholeStr.IndexOf("\"", pos_begin);
+                pos_right = wholeStr.IndexOf("\"", pos_left+1);
+                dropthing = wholeStr.Substring(pos_left+1, pos_right - pos_left - 1);
+                //Translate
+				dropthing_transed = trans(dropthing, true);
+                listBox1.Items.Add(dropthing_transed);
+
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItems.Count>0)
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+        }
+
+    }
+}
